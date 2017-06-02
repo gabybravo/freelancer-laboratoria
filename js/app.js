39d593a66af7912f7e38280f337fc259481d4dc0
Gabriela Bravo Faúndez <gbravo.visual@gmail.com>
2017-06-01 21:37:39 -0400

    Se validó el formulario de contacto y se agregó efecto hover a las imágenes del portafolio, faltó el modal y los efectos del menú o nav.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,31 @@
-/*
- * Archivo principal de JS
- */
+//Validación del formulario:
+var send = document.getElementsByClassName("btn")[0];
+send.addEventListener("click", function(e){
+ 	e.preventDefault();
+ 	
+ 	var nombre = document.getElementById("name").value;
+ 	var email = document.getElementById("email").value;
+ 	var phone = document.getElementById("phone").value;
+ 	var message = document.getElementById("message").value;
+
+ 	if(nombre == ""){
+ 		document.getElementById("name").value = "Ingrese su Nombre";
+ 	}	
+ 	if(email ==""){
+		document.getElementById("email").value = "Ingrese su Email";
+ 	}
+ 	if(phone ==""){
+		document.getElementById("phone").value = "Ingrese su Teléfono";
+ 	}
+ 	if(message == ""){
+		document.getElementById("message").value = "· Ingrese su Mensaje";
+	}
+
+ 	if(nombre != "" && email != "" && phone != "" && message != "") {
+ 		document.getElementById("name").value ="";
+ 		document.getElementById("email").value ="";
+ 		document.getElementById("phone").value ="";
+ 		document.getElementById("message").value ="";	
+ 	}		
+
+ })
